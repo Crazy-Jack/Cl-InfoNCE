6dd309b58ba00a6854f2806351961df9e48e772b
Tianqin Li <jacklitianqin@gmail.com>
2021-06-04 06:22:39 -0400

    add requirements

--- script/run_eval.sh
+++ script/run_eval.sh
@@ -3,7 +3,7 @@
 
 # define fixed variables
 dataset="imagenet100";
-latent_class="hier";
+instruction="hier";
 meta_file_train="meta_data_train.csv";
 
 ckpt=$1;
@@ -12,7 +12,7 @@ eval_epoch=$2;
 command="python ../clinfonce/main_linear.py --ckpt "$ckpt/ckpt_epoch_$eval_epoch.pth" \
 --learning_rate 0.3 \
 --lr_scheduling cosine \
---epochs 100 --latent_class $latent_class \
+--epochs 100 --instruction $instruction \
 --meta_file_train $meta_file_train \
 --method SupCon --batch_size 128 \
 --num_workers 10 \
